9ed2a39a10c484f777372a65b68fd17b1bba4b0b
17277 <17277@DESKTOP-07VJ3J8>
2022-01-17 16:32:25 -0500

    Add project files.

---
+++ GreedyAlgo.cpp
@@ -0,0 +1,35 @@
+// GreedyAlgo.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+void changeNeeded(double ammount[], double total[], double dollarAmmount);
+
+
+int main()
+{
+    double ammount[5] = {1.00, 0.25, 0.10, 0.05, 0.01};
+    double total[5] = {0, 0, 0, 0, 0};
+    double dollerAmmount;
+
+    std::cin >> dollerAmmount;
+
+    changeNeeded(ammount, total, dollerAmmount);
+
+    std::cout << "Dollars: " << total[0] << "\n";
+    std::cout << "quarters: " << total[1] << "\n";
+    std::cout << "dimes: " << total[2] <<"\n";
+    std::cout << "nickles: " << total[3] <<"\n";
+    std::cout << "pennies: " << total[4] <<"\n";
+
+}
+
+void changeNeeded(double ammount[], double total[], double dollarAmmount) {
+    for (int i = 0; i < 5; i++) {
+        while (dollarAmmount >= ammount[i]) {
+            total[i] += 1;
+            dollarAmmount -= ammount[i];
+            
+        }
+    }
+}
